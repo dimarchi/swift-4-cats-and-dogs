bfb48bb5c5fa3046bb80121eaa383d6911559072
Tarmo Turunen <->
2017-11-01 12:34:58 +0200

    Initial Commit

---
+++ sqlitetab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  sqlitetab
+//
+//  Created by Koulutus on 01/11/2017.
+//  Copyright © 2017 Koulutus. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
